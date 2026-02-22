3eb5079b0e5e25e3edcca633a5adc40dc68c7682
TazHinkle <hinkle.taz@gmail.com>
2026-02-22 13:00:56 -0700

    The map moves!

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,23 @@
 <script setup>
+import {ref, useTemplateRef, onMounted} from "vue";
+
+const mapLeft = ref(0);
+const mapTop = ref(0);
+const mapRef = useTemplateRef('mapRef')
+const mapSlide = function (event) {
+  if (!['ArrowLeft', 'ArrowRight', 'ArrowDown', 'ArrowUp'].includes(event.code)) {
+    return;
+  }
+  if (event.code === 'ArrowRight') {
+    mapLeft.value -= 2;
+  }
+  if (event.code === 'ArrowLeft') mapLeft.value += 2;
+}
+
+onMounted(() => {
+  // the event listener for this element will only work when the element is focused
+  mapRef.value.focus();
+})
 
 
 </script>
@@ -6,23 +25,35 @@
 <template>
   
   <div class="map-div">
-
-    <img id="map" src="/Sample.png"
-    width="2000"
-    height="1400"
+    <img class="map"
+      src="/Sample.png"
+      alt="the whole world"
+      ref="mapRef"
+      width="1800"
+      height="1200"
+      tabindex="0"
+      @keydown="mapSlide"
+      :style="{'left': + mapLeft + 'px'}"
     />
     
-    <div class="sprite1">
-
+    <div class="sprite-holder">
+      <div class="sprite1"></div>
       
-
     </div>
 
 
 
   </div>
-  
 
 </template>
 
-<style scoped></style>
+<style scoped>
+.map-div {
+  position: relative;
+  height: 1svh;
+}
+
+.map {
+  position: absolute;
+}
+</style>
